cde83d0525c40d96bf0352b290ee360a3de9c003
ZEWAAS <simon.wallner@student.htldornbirn.at>
2023-03-27 16:36:47 +0200

    comment box

---
+++ app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form');
+const commentsContainer = document.querySelector('#comments');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const comment = document.querySelector('#comment').value.trim();
+
+  if (comment === '') {
+    alert('Bitte geben Sie einen Kommentar ein.');
+    return;
+  }
+
+  // Kommentar-Element erstellen und hinzufügen
+  const commentElement = document.createElement('p');
+  commentElement.textContent = "Du:" + " " + comment;
+  commentElement.style.fontWeight = "bold";
+  commentsContainer.appendChild(commentElement);
+
+  // Textfeld leeren
+  document.querySelector('#comment').value = '';
+});
