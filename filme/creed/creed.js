07385606c9b2f4aea1ca04e7cef93efae6974d5d
ZEWAAS <simon.wallner@student.htldornbirn.at>
2023-04-11 11:26:56 +0200

    updated bottom-pages

--- filme/creed/creed.js
+++ filme/creed/creed.js
@@ -11,6 +11,8 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+
 //Kommentarbox
 const form = document.querySelector('form');
 const commentsContainer = document.querySelector('#comments');
